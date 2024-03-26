3807556c8bc5a1a50a6eb5496d664049a63c11f5
PavlosPo <pavlos.poulos@outlook.com>
2024-03-26 21:36:03 +0200

    Create Parameter Variables at the script

--- pytorch-experiments-3-fosi-adam/main.py
+++ pytorch-experiments-3-fosi-adam/main.py
@@ -1,6 +1,6 @@
 import torch
-import torchvision.transforms as transforms
 from torch.utils.data.dataloader import default_collate
+import torchvision.transforms as transforms
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, DataCollatorWithPadding, Trainer, TrainingArguments
 from torch.optim import Adam
 import torch.nn as nn
@@ -18,10 +18,16 @@ from transformers import AutoModelForSequenceClassification, AutoTokenizer, Data
 import subprocess
 
 # Load pre-trained BERT model and tokenizer
-bert_model_name = "bert-base-uncased"
-num_classes = 1
-bert_model = AutoModelForSequenceClassification.from_pretrained(bert_model_name, num_labels=num_classes)
-tokenizer = AutoTokenizer.from_pretrained(bert_model_name)
+BERT_MODEL_NAME = "bert-base-uncased"
+NUM_CLASSES = 1
+EPOCHS = 2
+BATCH_SIZE = 8
+DATASET_FROM = 'glue'
+DATASET_TASK = 'sst2'
+
+# Fetching pretrained model
+bert_model = AutoModelForSequenceClassification.from_pretrained(BERT_MODEL_NAME, num_labels=NUM_CLASSES)
+tokenizer = AutoTokenizer.from_pretrained(BERT_MODEL_NAME)
 
 class BertClassifier(nn.Module):
     def __init__(self, bert_model, num_classes):
@@ -35,7 +41,7 @@ class BertClassifier(nn.Module):
         return probability
 
 # Instantiate the classifier
-classifier = BertClassifier(bert_model, num_classes)
+classifier = BertClassifier(bert_model, NUM_CLASSES)
 
 # Optionally, move the model to a GPU device if available
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -48,15 +54,15 @@ def prepare_dataset(example):
 
 data_collator = DataCollatorWithPadding(tokenizer=tokenizer, padding=True)
 
-dataset = load_dataset('glue', 'sst2').map(prepare_dataset, batched=True)
-metric = evaluate.load("glue", "sst2")
+dataset = load_dataset(DATASET_FROM, DATASET_TASK).map(prepare_dataset, batched=True)
+metric = evaluate.load(DATASET_FROM, DATASET_TASK)
 
 # Split dataset into train and test sets, we use the train category because the test one has labels -1 only.
 train_dataset = dataset['train'].select(range(0,500)).remove_columns(['sentence', 'idx']).rename_column('label', 'labels')
 test_dataset = dataset['train'].select(range(500, 1000)).remove_columns(['sentence', 'idx']).rename_column('label', 'labels')
 
 # Define data loaders
-batch_size = 128
+batch_size = BATCH_SIZE
 trainloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=data_collator)
 testloader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True, collate_fn=data_collator)
 
@@ -84,10 +90,10 @@ opt_state = optimizer.init(params)
 
 # Train the model
 classifier.train()
-for epoch in range(2):
+for epoch in range(EPOCHS):
     progress_bar = tqdm(enumerate(trainloader, 1), total=len(trainloader))
     for i, data in progress_bar:
-        progress_bar.set_description(f'Epoch {epoch+1}/{2}, Step {i}/{len(trainloader)}')
+        progress_bar.set_description(f'Epoch {epoch+1}/{EPOCHS}, Step {i}/{len(trainloader)}')
 
         input_ids = data['input_ids'].squeeze().to(device)
         attention_mask = data['attention_mask'].squeeze().to(device)
@@ -133,7 +139,6 @@ with torch.no_grad():
         evaluation_results.append(evaluation_result)
         metric.add_batch(predictions=predicted_labels, references=labels)
 
-print(f'Epoch: {epoch}')
 results = metric.compute()
 print(f"Results: \n{results}\n")
 
@@ -143,14 +148,22 @@ print(f"\nLabels: {evaluation_results[0]['labels']}\n")
 print(f"\nPreds: {evaluation_results[0]['preds']}")
 print(f"\nPredicted Labels: {evaluation_results[0]['predicted_labels']}")
 
-def free_up_memory():
-    try:
-        # Execute shell commands to sync file system and drop page caches
-        subprocess.run(['sync'])
-        subprocess.run(['echo', '3'], capture_output=True, text=True, check=True, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print("Memory freed successfully.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error occurred while freeing memory: {e.stderr}")
+# Free up memory by deleting variables, tensors, and models
 
-
-free_up_memory()
+del tokenizer
+del bert_model
+del classifier
+del data_collator
+del dataset
+del metric
+del train_dataset
+del test_dataset
+del trainloader
+del testloader
+del optimizer
+del model
+del params
+del buffers
+del opt_state
+del evaluation_results
+del results
