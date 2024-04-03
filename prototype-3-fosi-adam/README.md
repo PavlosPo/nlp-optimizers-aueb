5b5441e053a3e98e06d39b977d6b78a28ecb6500
Pavlos Poulos <pavlos.poulos@outlook.com>
2024-04-03 22:17:16 +0000

    Mitigated from functorch to pytorch's func torch, results are shown in ./runs

--- prototype-3-fosi-adam/README.md
+++ prototype-3-fosi-adam/README.md
@@ -1,1 +1,6 @@
 Here we will try to fix the bad metrics (zeros and pure ones 0, 1) by running more examples, changing some code or both.
+
+**Important**
+Managed to mitigate to the new API from Pytorch, in order to use functional model.
+See [link1](https://pytorch.org/docs/stable/func.migrating.html#function-transforms) and [link2](https://gist.github.com/zou3519/7769506acc899d83ef1464e28f22e6cf).
+Note to self: In order to work you need to pass arguments as it is in the functional model, after every time is being updated with the new params and buffers !
--- prototype-3-fosi-adam/training.py
+++ prototype-3-fosi-adam/training.py
@@ -1,8 +1,9 @@
 import torch
 from tqdm import tqdm
 from fosi import fosi_adam_torch
-import functorch
+import copy
 import torchopt
+import functorch
 from torch.utils.data import DataLoader
 from torch import Tensor
 from typing import Tuple
@@ -24,8 +25,10 @@ class CustomTrainer:
         base_optimizer = torchopt.adam(lr=0.01)
         data = next(iter(self.train_loader))
         optimizer = fosi_adam_torch(base_optimizer, self.loss_fn, data, num_iters_to_approx_eigs=500, alpha=0.01)
-        self.functional_model, self.params, self.buffers = functorch.make_functional_with_buffers(model=self.original_model)
-        opt_state = optimizer.init(self.params)
+        self.functional_model, self.params, self.buffers = self.make_functional_with_buffers(self.original_model)
+        # self.functional_model, self.params, self.buffers = torch.func.functional_call(self.original_model, dict(self.original_model.named_parameters()))
+        
+        opt_state = optimizer.init(self.params)   
 
         self.original_model.train()
         for epoch in range(self.epochs):
@@ -40,6 +43,7 @@ class CustomTrainer:
                 attention_mask = data['attention_mask'].squeeze().to(self.device)
                 labels = data['labels'].squeeze().to(self.device)
 
+                # loss = self.loss_fn(self.functional_model, self.params, self.buffers, input_ids, attention_mask, labels)
                 loss = self.loss_fn(self.functional_model, self.params, self.buffers, input_ids, attention_mask, labels)
                 epoch_loss += loss.item()  # Accumulate loss for each batch
 
@@ -49,7 +53,9 @@ class CustomTrainer:
 
                 progress_bar.set_postfix(loss=loss.item())
 
-                preds = self.functional_model(params=self.params, buffers=self.buffers, input_ids=input_ids, attention_mask=attention_mask)
+                fmodel, _, __ = self.make_functional_with_buffers(mod=self.original_model, new_params_values=self.params, new_buffers_values=self.buffers)
+                preds = fmodel(input_ids=input_ids, attention_mask = attention_mask)
+
                 predictions = torch.round(preds).to(torch.float32)
                 epoch_preds.extend(predictions.detach().cpu().numpy())
                 epoch_labels.extend(labels.detach().cpu().numpy())
@@ -66,8 +72,14 @@ class CustomTrainer:
 
         return self.functional_model, self.params, self.buffers
 
+    # def loss_fn(self, functional_model: callable, params: Tuple[Tensor], buffers: Tuple[Tensor], input_ids: Tensor, attention_mask: Tensor, labels: Tensor) -> Tensor:
+    #     preds = functional_model(params=params, buffers=buffers, input_ids=input_ids, attention_mask=attention_mask)
+    #     loss = torch.nn.functional.binary_cross_entropy(preds.squeeze().to(torch.float32), labels.squeeze().to(torch.float32))
+    #     return loss
+
     def loss_fn(self, functional_model: callable, params: Tuple[Tensor], buffers: Tuple[Tensor], input_ids: Tensor, attention_mask: Tensor, labels: Tensor) -> Tensor:
-        preds = functional_model(params=params, buffers=buffers, input_ids=input_ids, attention_mask=attention_mask)
+        fmodel, _, __ = self.make_functional_with_buffers(mod=self.original_model, new_params_values=params, new_buffers_values=buffers)
+        preds = fmodel(input_ids=input_ids, attention_mask = attention_mask)
         loss = torch.nn.functional.binary_cross_entropy(preds.squeeze().to(torch.float32), labels.squeeze().to(torch.float32))
         return loss
 
@@ -87,7 +99,8 @@ class CustomTrainer:
                 attention_mask = data['attention_mask'].squeeze().to(self.device)
                 labels = data['labels'].squeeze().to(self.device)
 
-                preds = self.functional_model(params=self.params, buffers=self.buffers, input_ids=input_ids, attention_mask=attention_mask)
+                fmodel, _, __ = self.make_functional_with_buffers(mod=self.original_model, new_params_values=self.params, new_buffers_values=self.buffers)
+                preds = fmodel(input_ids=input_ids, attention_mask = attention_mask)
                 loss = self.loss_fn(self.functional_model, self.params, self.buffers, input_ids, attention_mask, labels)
                 val_loss += loss.item()  # Accumulate validation loss
 
@@ -115,7 +128,8 @@ class CustomTrainer:
                 attention_mask = data['attention_mask'].squeeze().to(self.device)
                 labels = data['labels'].squeeze().to(self.device)
 
-                preds = self.functional_model(params=self.params, buffers=self.buffers, input_ids=input_ids, attention_mask=attention_mask)
+                fmodel, _, __ = self.make_functional_with_buffers(mod=self.original_model, new_params_values=self.params, new_buffers_values=self.buffers)
+                preds = fmodel(input_ids=input_ids, attention_mask = attention_mask)
                 loss = self.loss_fn(self.functional_model, self.params, self.buffers, input_ids, attention_mask, labels)
                 test_loss += loss.item()  # Accumulate test loss
 
@@ -130,3 +144,81 @@ class CustomTrainer:
         self.logger.log_test_metrics(test_loss, test_preds, test_labels)
 
         return self.functional_model, self.params, self.buffers
+
+    def make_functional(self, mod, new_params_values=None, disable_autograd_tracking=False):
+        params_dict = dict(mod.named_parameters())
+        params_names = params_dict.keys()
+        params_values = tuple(params_dict.values())
+        
+        stateless_mod = copy.deepcopy(mod)
+        stateless_mod.to('meta')
+
+        # This remains Unchanged and not used in the code
+        def fmodel(new_params_values=new_params_values, *args, **kwargs):
+            if new_params_values is None:
+                # This is the first call to the functional model
+                new_params_values = params_values
+            new_params_dict = {name: value for name, value in zip(params_names, new_params_values)}
+            return torch.func.functional_call(stateless_mod, new_params_dict, args, kwargs)
+    
+        if disable_autograd_tracking:
+            params_values = torch.utils._pytree.tree_map(torch.Tensor.detach, params_values)
+        return fmodel, params_values
+
+    def make_functional_with_buffers(self, mod, new_params_values=None, new_buffers_values=None, disable_autograd_tracking=False):
+
+        """
+        Given a module, return a functional version of the module that can be called with
+        the parameters and buffers as arguments. This is useful for optimization libraries
+        that require a functional interface to the model.
+
+        Args:
+            mod: A PyTorch module.
+            disable_autograd_tracking: If True, the parameters will be detached from the computation graph.
+
+        Returns:
+            A tuple (fmodel, params, buffers), where:
+            - fmodel is a functional version of the module.
+            - params is a tuple of the parameters of the module.
+            - buffers is a tuple of the buffers of the module.
+        
+        This was taken from the official PyTorch library.
+        Repo Link: https://gist.github.com/zou3519/7769506acc899d83ef1464e28f22e6cf
+        Original Docs: https://pytorch.org/docs/stable/func.migrating.html#function-transforms
+        """
+        params_dict = dict(mod.named_parameters())
+        params_names = params_dict.keys()
+        params_values = tuple(params_dict.values())
+
+        buffers_dict = dict(mod.named_buffers())
+        buffers_names = buffers_dict.keys()
+        buffers_values = tuple(buffers_dict.values())
+        
+        stateless_mod = copy.deepcopy(mod)
+        stateless_mod.to('meta')
+
+        # def fmodel(new_params_values=new_buffers_values, new_buffers_values=new_buffers_values, *args, **kwargs):
+        #     if new_params_values is None:
+        #         # This is the first call to the functional model
+        #         new_params_values = params_values
+        #     if new_buffers_values is None:
+        #         # This is the first call to the functional model
+        #         new_buffers_values = buffers_values
+        #     new_params_dict = {name: value for name, value in zip(params_names, new_params_values)}
+        #     new_buffers_dict = {name: value for name, value in zip(buffers_names, new_buffers_values)}
+        #     return torch.func.functional_call(stateless_mod, (new_params_dict, new_buffers_dict), args, kwargs)
+
+        def fmodel(new_params_values=new_params_values, new_buffers_values=new_buffers_values, *args, **kwargs):
+            if new_params_values is None:
+                # This is the first call to the functional model
+                new_params_values = params_values
+            if new_buffers_values is None:
+                # This is the first call to the functional model
+                new_buffers_values = buffers_values
+            new_params_dict = {name: value for name, value in zip(params_names, new_params_values)}
+            new_buffers_dict = {name: value for name, value in zip(buffers_names, new_buffers_values)}
+            return torch.func.functional_call(stateless_mod, (new_params_dict, new_buffers_dict), args=args, kwargs=kwargs)
+
+        if disable_autograd_tracking:
+            params_values = torch.utils._pytree.tree_map(torch.Tensor.detach, params_values)
+        return fmodel, params_values, buffers_values
