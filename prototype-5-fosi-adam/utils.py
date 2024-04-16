9c5066461a421cbe514dd874116903f92dbdecfd
PavlosPo <pavlos.poulos@outlook.com>
2024-04-16 12:25:39 +0300

    XLA is not possible, updating in order to return to cpu or cuda

--- prototype-5-fosi-adam/utils.py
+++ prototype-5-fosi-adam/utils.py
@@ -1,6 +1,6 @@
 import torch
-import torch_xla    # This is the PyTorch-XLA package that works onluy in the server not need to install in the local machine
-import torch_xla.core.xla_model as xm # This is the PyTorch-XLA package that works onluy in the server not need to install in the local machine
+# import torch_xla    # This is the PyTorch-XLA package that works onluy in the server not need to install in the local machine
+# import torch_xla.core.xla_model as xm # This is the PyTorch-XLA package that works onluy in the server not need to install in the local machine
 import random
 import numpy as np
 from icecream import ic
@@ -33,8 +33,8 @@ def set_seed(seed: int):
 def select_device():
     if torch.cuda.is_available():
         device = torch.device("cuda")
-    elif xm.xla_device():
-        device = torch.device("xla")
+    # elif xm.xla_device():
+    #     device = torch.device("xla")
     else:
         device = torch.device("cpu")
     ic(device)
