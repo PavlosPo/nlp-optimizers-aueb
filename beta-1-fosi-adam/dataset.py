4218af49a81e96610c65eec080ce71873e6ab5af
PavlosPo <pavlos.poulos@outlook.com>
2024-04-19 14:54:18 +0300

    Created the first beta version

---
+++ beta-1-fosi-adam/dataset.py
@@ -0,0 +1,74 @@
+from transformers import AutoTokenizer, DataCollatorWithPadding
+from torch.utils.data import DataLoader
+import torch
+from datasets import load_dataset, concatenate_datasets
+from typing import Tuple
+from icecream import ic
+
+torch.set_default_dtype(torch.float32)
+
+class CustomDataLoader:
+    def __init__(self, 
+                dataset_from :str = "glue",
+                dataset_task : str = "cola", 
+                model_name: str = "distilbert-base-uncased",
+                tokenizer: AutoTokenizer = None, 
+                seed_num: int = 1, range_to_select: int | None = None, batch_size: int = 8) -> None:
+      self.dataset_task = dataset_task
+      self.dataset_from = dataset_from
+      self.model_name = model_name
+      self.seed_num = seed_num
+      self.range_to_select = range_to_select
+      self.batch_size = batch_size
+      self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+      self.data_collator = DataCollatorWithPadding(tokenizer=self.tokenizer, padding=True)
+      # self.metric = evaluate.load(self.dataset_from, self.dataset_task)
+
+      self.GLUE_TASKS = ["cola", "mnli", "mnli-mm", "mrpc", "qnli", "qqp", "rte", "sst2", "stsb", "wnli"]
+      self.task_to_keys = {
+          "cola": ("sentence", None),
+          "mnli": ("premise", "hypothesis"),
+          "mnli-mm": ("premise", "hypothesis"),
+          "mrpc": ("sentence1", "sentence2"),
+          "qnli": ("question", "sentence"),
+          "qqp": ("question1", "question2"),
+          "rte": ("sentence1", "sentence2"),
+          "sst2": ("sentence", None),
+          "stsb": ("sentence1", "sentence2"),
+          "wnli": ("sentence1", "sentence2"),
+      }
+      
+      self.sentence1_key, self.sentence2_key = self.task_to_keys[self.dataset_task]
+      # ic(self.sentence1_key, self.sentence2_key)
+      # ic(self.task_to_keys[self.dataset_task])
+
+    def get_custom_data_loaders(self) -> Tuple[DataLoader, DataLoader, DataLoader]:
+      dataset = load_dataset(self.dataset_from, self.dataset_task).map(self._prepare_dataset, batched=True)
+      dataset = concatenate_datasets([dataset["train"], dataset["validation"]]).train_test_split(test_size=0.1666666666666, seed=self.seed_num, stratify_by_column='label')
+      dataset['validation'] = dataset['test'].train_test_split(test_size=0.5, seed=self.seed_num, stratify_by_column='label')['train']
+      dataset['test'] = dataset['test'].train_test_split(test_size=0.5, seed=self.seed_num, stratify_by_column='label')['test']
+      # ic(dataset.keys())
+      # ic(len(dataset['train']), len(dataset['validation']), len(dataset['test']))
+      # ic(dataset['train'].select(range(5)), dataset['validation'].select(range(5)), dataset['test'].select(range(5)))
+
+      if self.range_to_select is None:  # Use the entire dataset
+        train_dataset = dataset['train'].remove_columns(['idx'] + [col for col in dataset["train"].column_names if col in self.task_to_keys[self.dataset_task]]).rename_column('label', 'labels')
+        val_dataset = dataset['validation'].remove_columns(['idx'] + [col for col in dataset["validation"].column_names if col in self.task_to_keys[self.dataset_task]]).rename_column('label', 'labels')
+        test_dataset = dataset['test'].remove_columns(['idx'] + [col for col in dataset["test"].column_names if col in self.task_to_keys[self.dataset_task]]).rename_column('label', 'labels')
+      else: # Use a subset of the dataset
+        train_dataset = dataset['train'].select(range(self.range_to_select)).remove_columns(['idx'] + [col for col in dataset["train"].column_names if col in self.task_to_keys[self.dataset_task]]).rename_column('label', 'labels')
+        val_dataset = dataset['validation'].select(range(self.range_to_select)).remove_columns(['idx'] + [col for col in dataset["validation"].column_names if col in self.task_to_keys[self.dataset_task]]).rename_column('label', 'labels')
+        test_dataset = dataset['test'].select(range(self.range_to_select)).remove_columns(['idx'] + [col for col in dataset["test"].column_names if col in self.task_to_keys[self.dataset_task]]).rename_column('label', 'labels')
+
+      train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True, collate_fn=self.data_collator)
+      val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=self.batch_size, shuffle=True, collate_fn=self.data_collator)
+      test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=self.batch_size, shuffle=True, collate_fn=self.data_collator)
+
+      # ic(len(train_loader), len(val_loader), len(test_loader))
+      return train_loader, val_loader, test_loader
+    
+    def _prepare_dataset(self, examples) -> dict:
+      if self.sentence2_key is None:
+          return self.tokenizer(examples[self.sentence1_key], truncation=True)
+      # ic(examples[self.sentence1_key][:10], examples[self.sentence2_key][:10])
+      return self.tokenizer(examples[self.sentence1_key], examples[self.sentence2_key], truncation=True)
---
+++ beta-1-fosi-adam/logger.py
@@ -0,0 +1,198 @@
+from torch.utils.tensorboard import SummaryWriter  # Import SummaryWriter for TensorBoard logging
+from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score, mean_absolute_error, roc_auc_score, matthews_corrcoef
+import torch
+import torch.nn.functional as F
+import numpy as np
+from icecream import ic
+
+class CustomLogger:
+    def __init__(self, len_train_loader: int, len_validation_loader: int, len_test_loader: int) -> None:
+        
+        self.len_train_loader = len_train_loader
+        self.len_validation_loader = len_validation_loader
+        self.len_test_loader = len_test_loader
+
+        self.train_losses = []
+        self.train_f1_scores = []
+        self.train_accuracies = []
+        self.train_precisions = []
+        self.train_recalls = []
+        self.train_maes = []
+        self.train_auc_roc = []
+        self.train_mcc = []
+
+        self.validation_losses = []
+        self.validation_f1_scores = []
+        self.validation_accuracies = []
+        self.validation_precisions = []
+        self.validation_recalls = []
+        self.validation_maes = []
+        self.validation_auc_roc = []
+        self.validation_mcc = []
+
+        self.test_losses = []
+        self.test_f1_scores = []
+        self.test_accuracies = []
+        self.test_precisions = []
+        self.test_recalls = []
+        self.test_maes = []
+        self.test_auc_roc = []
+        self.test_mcc = []
+        self.writer = SummaryWriter()  # Initialize SummaryWriter for TensorBoard logging
+
+    def custom_log(self, epoch, batch_idx, loss, outputs, labels):
+        # Clone outputs and labels to avoid modifying the original tensors
+        # ic(outputs)
+        outputs = outputs.clone().detach().cpu().numpy() if torch.is_tensor(outputs) else np.array(outputs)
+        labels = labels.clone().detach().cpu().numpy() if torch.is_tensor(labels) else np.array(labels)
+        # Apply softmax to the outputs and then take argmax
+        outputs_softmax = F.softmax(torch.tensor(outputs), dim=1).numpy()
+        # ic(outputs_softmax)
+        outputs_argmax = np.argmax(outputs_softmax, axis=1)
+        ic(outputs_argmax)
+        ic(labels)
+
+        # Calculate metrics
+        f1 = f1_score(labels, outputs_argmax, average='weighted')
+        accuracy = accuracy_score(labels, outputs_argmax)
+        precision = precision_score(labels, outputs_argmax, average='weighted', zero_division=0)
+        recall = recall_score(labels, outputs_argmax, average='weighted')
+        mae = mean_absolute_error(labels, outputs_argmax)
+        try:
+            auc_roc = roc_auc_score(labels, outputs_argmax, average='weighted', multi_class='ovr')
+        except ValueError:
+            auc_roc = 0.0
+        mcc = matthews_corrcoef(labels, outputs_argmax)
+
+
+        global_step = epoch * self.len_train_loader + batch_idx
+
+        # Log metrics for the current batch
+        self.writer.add_scalar('Loss/Train', loss, global_step=global_step)
+        self.writer.add_scalar('F1_Score/Train', f1, global_step=global_step)
+        self.writer.add_scalar('Accuracy/Train', accuracy, global_step=global_step)
+        self.writer.add_scalar('Precision/Train', precision, global_step=global_step)
+        self.writer.add_scalar('Recall/Train', recall, global_step=global_step)
+        self.writer.add_scalar('MAE/Train', mae, global_step=global_step )
+        self.writer.add_scalar('AUC_ROC/Train', auc_roc, global_step=global_step)
+        self.writer.add_scalar('MCC/Train', mcc, global_step=global_step)
+
+        # print(f"Train Epoch {epoch}: Loss: {loss}, F1 Score: {f1}, Accuracy: {accuracy}, Precision: {precision}, Recall: {recall}, MAE: {mae}, AUC ROC: {auc_roc}, MCC: {mcc}")
+
+    def custom_log_validation(self, epoch, batch_idx, loss, outputs, labels):
+        # Clone outputs and labels to avoid modifying the original tensors
+        # ic(outputs)
+        outputs = outputs.clone().detach().cpu().numpy() if torch.is_tensor(outputs) else np.array(outputs)
+        labels = labels.clone().detach().cpu().numpy() if torch.is_tensor(labels) else np.array(labels)
+        
+        # Apply softmax to the outputs and then take argmax
+        outputs_softmax = F.softmax(torch.tensor(outputs), dim=1).numpy()
+        # ic(outputs_softmax)
+        outputs_argmax = np.argmax(outputs_softmax, axis=1)
+        ic(outputs_argmax)
+        ic(labels)
+
+        # Calculate metrics
+        f1 = f1_score(labels, outputs_argmax, average='weighted')
+        accuracy = accuracy_score(labels, outputs_argmax)
+        precision = precision_score(labels, outputs_argmax, average='weighted')
+        recall = recall_score(labels, outputs_argmax, average='weighted')
+        mae = mean_absolute_error(labels, outputs_argmax)
+        try:
+            auc_roc = roc_auc_score(labels, outputs_argmax, average='weighted', multi_class='ovr')
+        except ValueError:
+            auc_roc = 0.0
+        mcc = matthews_corrcoef(labels, outputs_argmax)
+
+
+        global_step = epoch * self.len_validation_loader + batch_idx
+
+        # Log metrics for the current batch
+        self.writer.add_scalar('Loss/Validation', loss, global_step=global_step)
+        self.writer.add_scalar('F1_Score/Validation', f1, global_step=global_step)
+        self.writer.add_scalar('Accuracy/Validation', accuracy, global_step=global_step)
+        self.writer.add_scalar('Precision/Validation', precision, global_step=global_step)
+        self.writer.add_scalar('Recall/Validation', recall, global_step=global_step)
+        self.writer.add_scalar('MAE/Validation', mae, global_step=global_step )
+        self.writer.add_scalar('AUC_ROC/Validation', auc_roc, global_step=global_step)
+        self.writer.add_scalar('MCC/Validation', mcc, global_step=global_step)
+
+    def custom_log_test(self, batch_idx, loss, outputs, labels, epoch=0):
+        # Clone outputs and labels to avoid modifying the original tensors
+        # ic(outputs)
+        outputs = outputs.clone().detach().cpu().numpy() if torch.is_tensor(outputs) else np.array(outputs)
+        labels = labels.clone().detach().cpu().numpy() if torch.is_tensor(labels) else np.array(labels)
+        
+        # Apply softmax to the outputs and then take argmax
+        outputs_softmax = F.softmax(torch.tensor(outputs), dim=1).numpy()
+        # ic(outputs_softmax)
+        outputs_argmax = np.argmax(outputs_softmax, axis=1)
+        ic(outputs_argmax)
+        ic(labels)
+
+        global_step = epoch * self.len_test_loader + batch_idx
+
+        # Calculate metrics
+        f1 = f1_score(labels, outputs_argmax, average='weighted')
+        accuracy = accuracy_score(labels, outputs_argmax)
+        precision = precision_score(labels, outputs_argmax, average='weighted')
+        recall = recall_score(labels, outputs_argmax, average='weighted')
+        mae = mean_absolute_error(labels, outputs_argmax)
+        try:
+            auc_roc = roc_auc_score(labels, outputs_argmax, average='weighted', multi_class='ovr')
+        except ValueError:
+            auc_roc = 0.0
+        mcc = matthews_corrcoef(labels, outputs_argmax)
+
+        # Log metrics for the current batch
+        self.writer.add_scalar('Loss/Test', loss, global_step=global_step)
+        self.writer.add_scalar('F1_Score/Test', f1, global_step=global_step)
+        self.writer.add_scalar('Accuracy/Test', accuracy, global_step=global_step)
+        self.writer.add_scalar('Precision/Test', precision, global_step=global_step)
+        self.writer.add_scalar('Recall/Test', recall, global_step=global_step)
+        self.writer.add_scalar('MAE/Test', mae, global_step=global_step)
+        self.writer.add_scalar('AUC_ROC/Test', auc_roc, global_step=global_step)
+        self.writer.add_scalar('MCC/Test', mcc, global_step=global_step)
+
+
+    def log_additional_information(self, dataset_name: str, 
+                         dataset_task: str, 
+                         dataset_size: int, 
+                         test_dataset_size: int,
+                         validation_dataset_size: int,
+                         train_dataset_size: int,
+                         num_classes: int, 
+                         optimizer_name: str, 
+                         base_optimizer_name: str, 
+                         learning_rate_of_base_optimizer: float, 
+                         batch_size: int, 
+                         epochs: int, 
+                         k_approx: int, 
+                         num_of_optimizer_iterations: int, 
+                         seed_num: int,
+                         range_to_select: int):
+        # Log dataset information
+        self.writer.add_text('Dataset Information', f'Dataset Name: {dataset_name}')
+        self.writer.add_text('Dataset Information', f'Dataset Task: {dataset_task}')
+        self.writer.add_text('Dataset Information', f'All Dataset Size: {dataset_size}')
+        self.writer.add_text('Dataset Information', f'Test Dataset Size: {test_dataset_size}')
+        self.writer.add_text('Dataset Information', f'Validation Dataset Size: {validation_dataset_size}')
+        self.writer.add_text('Dataset Information', f'Train Dataset Size: {train_dataset_size}')
+        self.writer.add_text('Dataset Information', f'Number of Classes: {num_classes}')
+        self.writer.add_text('Dataset Information', f'Batch Size: {batch_size}')
+        self.writer.add_text('Dataset Information', f'Epochs: {epochs}')
+        self.writer.add_text('Optimization Information', f'Optimizer Name: {optimizer_name}')
+        self.writer.add_text('Optimization Information', f'Base Optimizer Name: {base_optimizer_name}')
+        self.writer.add_text('Optimization Information', f'Learning Rate of Base Optimizer: {learning_rate_of_base_optimizer}')
+        self.writer.add_text('Optimization Information', f'Number of Max Eigenvalues to Approximate: {k_approx}')
+        self.writer.add_text('Optimization Information', f'Number of Optimizer Iterations: {num_of_optimizer_iterations}')
+        # if range to select is None then the entire dataset is used
+        if range_to_select is None:
+            range_to_select = 'All Dataset'
+        self.writer.add_text('Optimization Information', f'Range to Select: {range_to_select}')
+        self.writer.add_text('Optimization Information', f'Seed Number: {seed_num}')
+
+
+    def close(self):
+        self.writer.close() # Close the SummaryWriter
+        
