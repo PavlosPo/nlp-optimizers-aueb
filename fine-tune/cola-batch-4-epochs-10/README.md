fe0428c06437dcf31317d654c40c739188058636
Pavlos Poulos - Linux Server EU West <pavlos.poulos@outlook.com>
2024-05-01 16:08:01 +0000

    Deleted some trials

--- fine-tune/cola-batch-4-epochs-10/README.md
+++
@@ -1,7 +0,0 @@
-## fine_tuning_database.db 
-is complete:
-* This means that we can get the best learning parameters out of 100 trials of dataset 
-* Cola
-* Seed Sum 1 
-* Training Epochs 2
-* Batch Size 128
--- fine-tune/cola-batch-4-epochs-10/fosi/jax_optim/__init__.py
+++
@@ -1,4 +0,0 @@
-from .extreme_spectrum_estimation import get_ese_fn
-from .fosi_optimizer import fosi, fosi_adam, fosi_momentum, fosi_sgd, fosi_nesterov
-
-__all__ = ['get_ese_fn', 'fosi', 'fosi_adam', 'fosi_momentum', 'fosi_sgd', 'fosi_nesterov']
